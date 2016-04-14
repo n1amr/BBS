d4e21d2d22af43ddc73f16243291765c506605d5
Amr Alaa <n1amr1@gmail.com>
2016-04-15 01:35:33 +0200

    Book model

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
-public static void main(String[] args) {
-  System.out.println("-------------");
-  System.out.println("|  Welcome! |");
-  System.out.println("-------------");
-}
+	public static void main(String[] args) {
+		System.out.println("-------------");
+		System.out.println("|  Welcome! |");
+		System.out.println("-------------");
+	}
 }
